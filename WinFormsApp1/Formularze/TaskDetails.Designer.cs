de923a8e111d67f43fb6b7507f5a635ec3a12ccf
Marcin Zawadzki <nicram01@tlen.pl>
2024-05-25 17:53:12 +0200

    Dodaj pliki projektów.

---
+++ WinFormsApp1/Formularze/TaskDetails.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace ListaZadan.Formularze
+{
+    partial class TaskDetails
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblNumerText = new Label();
+            lblNumer = new Label();
+            lblTytulText = new Label();
+            lblOpisText = new Label();
+            tbTitle = new TextBox();
+            cbIsReady = new CheckBox();
+            tbDescription = new TextBox();
+            btnSave = new Button();
+            SuspendLayout();
+            // 
+            // lblNumerText
+            // 
+            lblNumerText.AutoSize = true;
+            lblNumerText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblNumerText.Location = new Point(8, 9);
+            lblNumerText.Name = "lblNumerText";
+            lblNumerText.Size = new Size(81, 25);
+            lblNumerText.TabIndex = 0;
+            lblNumerText.Text = "Numer:";
+            // 
+            // lblNumer
+            // 
+            lblNumer.AutoSize = true;
+            lblNumer.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Bold, GraphicsUnit.Point);
+            lblNumer.Location = new Point(82, 9);
+            lblNumer.Name = "lblNumer";
+            lblNumer.Size = new Size(51, 25);
+            lblNumer.TabIndex = 1;
+            lblNumer.Text = "123";
+            // 
+            // lblTytulText
+            // 
+            lblTytulText.AutoSize = true;
+            lblTytulText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTytulText.Location = new Point(24, 49);
+            lblTytulText.Name = "lblTytulText";
+            lblTytulText.Size = new Size(65, 25);
+            lblTytulText.TabIndex = 2;
+            lblTytulText.Text = "Tytuł:";
+            // 
+            // lblOpisText
+            // 
+            lblOpisText.AutoSize = true;
+            lblOpisText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblOpisText.Location = new Point(27, 104);
+            lblOpisText.Name = "lblOpisText";
+            lblOpisText.Size = new Size(62, 25);
+            lblOpisText.TabIndex = 3;
+            lblOpisText.Text = "Opis:";
+            // 
+            // tbTitle
+            // 
+            tbTitle.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            tbTitle.Location = new Point(89, 46);
+            tbTitle.Name = "tbTitle";
+            tbTitle.Size = new Size(410, 31);
+            tbTitle.TabIndex = 4;
+            // 
+            // cbIsReady
+            // 
+            cbIsReady.AutoSize = true;
+            cbIsReady.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            cbIsReady.Location = new Point(508, 47);
+            cbIsReady.Name = "cbIsReady";
+            cbIsReady.Size = new Size(202, 29);
+            cbIsReady.TabIndex = 5;
+            cbIsReady.Text = "Czy zakończone?";
+            cbIsReady.UseVisualStyleBackColor = true;
+            // 
+            // tbDescription
+            // 
+            tbDescription.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            tbDescription.Location = new Point(89, 101);
+            tbDescription.Multiline = true;
+            tbDescription.Name = "tbDescription";
+            tbDescription.Size = new Size(619, 77);
+            tbDescription.TabIndex = 6;
+            // 
+            // btnSave
+            // 
+            btnSave.Font = new Font("Microsoft Sans Serif", 60F, FontStyle.Regular, GraphicsUnit.Point);
+            btnSave.Location = new Point(730, 36);
+            btnSave.Name = "btnSave";
+            btnSave.Size = new Size(131, 142);
+            btnSave.TabIndex = 7;
+            btnSave.Text = "💾";
+            btnSave.UseVisualStyleBackColor = true;
+            // 
+            // TaskDetails
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(884, 218);
+            Controls.Add(btnSave);
+            Controls.Add(tbDescription);
+            Controls.Add(cbIsReady);
+            Controls.Add(tbTitle);
+            Controls.Add(lblOpisText);
+            Controls.Add(lblTytulText);
+            Controls.Add(lblNumer);
+            Controls.Add(lblNumerText);
+            FormBorderStyle = FormBorderStyle.FixedToolWindow;
+            Name = "TaskDetails";
+            Text = "Zadanie";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblNumerText;
+        private Label lblNumer;
+        private Label lblTytulText;
+        private Label lblOpisText;
+        private TextBox tbTitle;
+        private CheckBox cbIsReady;
+        private TextBox tbDescription;
+        private Button btnSave;
+    }
+}
---
+++ WinFormsApp1/Kontrolki/LoginControl.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace ListaZadan.Kontrolki
+{
+    partial class LoginControl
+    {
+        /// <summary> 
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod wygenerowany przez Projektanta składników
+
+        /// <summary> 
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować 
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            lblTytulText = new Label();
+            lblLinia = new Label();
+            lblLoginText = new Label();
+            lblHasloText = new Label();
+            tbLogin = new TextBox();
+            tbHaslo = new TextBox();
+            lblWalidacjaLogowania = new Label();
+            btnZaloguj = new Button();
+            btnZalozKonto = new Button();
+            SuspendLayout();
+            // 
+            // lblTytulText
+            // 
+            lblTytulText.AutoSize = true;
+            lblTytulText.Font = new Font("Microsoft Sans Serif", 27.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTytulText.Location = new Point(147, 36);
+            lblTytulText.Name = "lblTytulText";
+            lblTytulText.Size = new Size(210, 42);
+            lblTytulText.TabIndex = 0;
+            lblTytulText.Text = "Lista zadań";
+            // 
+            // lblLinia
+            // 
+            lblLinia.AutoSize = true;
+            lblLinia.Font = new Font("Microsoft Sans Serif", 8.25F, FontStyle.Regular, GraphicsUnit.Point);
+            lblLinia.Location = new Point(51, 85);
+            lblLinia.Name = "lblLinia";
+            lblLinia.Size = new Size(403, 13);
+            lblLinia.TabIndex = 1;
+            lblLinia.Text = "__________________________________________________________________";
+            // 
+            // lblLoginText
+            // 
+            lblLoginText.AutoSize = true;
+            lblLoginText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblLoginText.Location = new Point(51, 157);
+            lblLoginText.Name = "lblLoginText";
+            lblLoginText.Size = new Size(71, 25);
+            lblLoginText.TabIndex = 2;
+            lblLoginText.Text = "Login:";
+            lblLoginText.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // lblHasloText
+            // 
+            lblHasloText.AutoSize = true;
+            lblHasloText.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblHasloText.Location = new Point(49, 199);
+            lblHasloText.Name = "lblHasloText";
+            lblHasloText.Size = new Size(73, 25);
+            lblHasloText.TabIndex = 3;
+            lblHasloText.Text = "Hasło:";
+            lblHasloText.TextAlign = ContentAlignment.MiddleRight;
+            // 
+            // tbLogin
+            // 
+            tbLogin.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            tbLogin.Location = new Point(129, 154);
+            tbLogin.Name = "tbLogin";
+            tbLogin.Size = new Size(225, 31);
+            tbLogin.TabIndex = 4;
+            // 
+            // tbHaslo
+            // 
+            tbHaslo.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            tbHaslo.Location = new Point(129, 196);
+            tbHaslo.Name = "tbHaslo";
+            tbHaslo.Size = new Size(225, 31);
+            tbHaslo.TabIndex = 5;
+            // 
+            // lblWalidacjaLogowania
+            // 
+            lblWalidacjaLogowania.AutoSize = true;
+            lblWalidacjaLogowania.Font = new Font("Microsoft Sans Serif", 15.75F, FontStyle.Regular, GraphicsUnit.Point);
+            lblWalidacjaLogowania.ForeColor = Color.Red;
+            lblWalidacjaLogowania.Location = new Point(105, 264);
+            lblWalidacjaLogowania.Name = "lblWalidacjaLogowania";
+            lblWalidacjaLogowania.Size = new Size(295, 25);
+            lblWalidacjaLogowania.TabIndex = 6;
+            lblWalidacjaLogowania.Text = "Nieprawidłowy login lub hasło";
+            lblWalidacjaLogowania.TextAlign = ContentAlignment.MiddleCenter;
+            lblWalidacjaLogowania.Visible = false;
+            // 
+            // btnZaloguj
+            // 
+            btnZaloguj.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnZaloguj.Location = new Point(140, 347);
+            btnZaloguj.Name = "btnZaloguj";
+            btnZaloguj.Size = new Size(225, 51);
+            btnZaloguj.TabIndex = 7;
+            btnZaloguj.Text = "Zaloguj";
+            btnZaloguj.UseVisualStyleBackColor = true;
+            // 
+            // btnZalozKonto
+            // 
+            btnZalozKonto.Font = new Font("Microsoft Sans Serif", 20.25F, FontStyle.Regular, GraphicsUnit.Point);
+            btnZalozKonto.Location = new Point(140, 416);
+            btnZalozKonto.Name = "btnZalozKonto";
+            btnZalozKonto.Size = new Size(225, 51);
+            btnZalozKonto.TabIndex = 8;
+            btnZalozKonto.Text = "Załóż konto";
+            btnZalozKonto.UseVisualStyleBackColor = true;
+            // 
+            // LoginControl
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnZalozKonto);
+            Controls.Add(btnZaloguj);
+            Controls.Add(lblWalidacjaLogowania);
+            Controls.Add(tbHaslo);
+            Controls.Add(tbLogin);
+            Controls.Add(lblHasloText);
+            Controls.Add(lblLoginText);
+            Controls.Add(lblLinia);
+            Controls.Add(lblTytulText);
+            Name = "LoginControl";
+            Size = new Size(515, 680);
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label lblTytulText;
+        private Label lblLinia;
+        private Label lblLoginText;
+        private Label lblHasloText;
+        private TextBox tbLogin;
+        private TextBox tbHaslo;
+        private Label lblWalidacjaLogowania;
+        private Button btnZaloguj;
+        private Button btnZalozKonto;
+    }
+}
