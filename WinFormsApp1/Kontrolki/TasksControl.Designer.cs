8edc7ae58e58ef3b857ff8b9b6f2e6ba108b9cf3
Marcin Zawadzki <nicram01@tlen.pl>
2024-05-26 15:56:44 +0200

    next paty of changes

--- WinFormsApp1/Kontrolki/TasksControl.Designer.cs
+++ WinFormsApp1/Kontrolki/TasksControl.Designer.cs
@@ -28,10 +28,83 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            lblTytul = new Label();
+            lblZalogowanyText = new Label();
+            lblZalogowany = new Label();
+            button1 = new Button();
+            btnWyloguj = new Button();
+            SuspendLayout();
+            // 
+            // lblTytul
+            // 
+            lblTytul.AutoSize = true;
+            lblTytul.Font = new Font("Comic Sans MS", 28F, FontStyle.Regular, GraphicsUnit.Point);
+            lblTytul.Location = new Point(4, 4);
+            lblTytul.Name = "lblTytul";
+            lblTytul.Size = new Size(202, 79);
+            lblTytul.TabIndex = 0;
+            lblTytul.Text = "TODO";
+            // 
+            // lblZalogowanyText
+            // 
+            lblZalogowanyText.AutoSize = true;
+            lblZalogowanyText.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Regular, GraphicsUnit.Point);
+            lblZalogowanyText.Location = new Point(216, 34);
+            lblZalogowanyText.Name = "lblZalogowanyText";
+            lblZalogowanyText.Size = new Size(240, 36);
+            lblZalogowanyText.TabIndex = 1;
+            lblZalogowanyText.Text = "Zalogowany jako";
+            // 
+            // lblZalogowany
+            // 
+            lblZalogowany.AutoSize = true;
+            lblZalogowany.Font = new Font("Microsoft Sans Serif", 15F, FontStyle.Bold, GraphicsUnit.Point);
+            lblZalogowany.Location = new Point(446, 34);
+            lblZalogowany.Name = "lblZalogowany";
+            lblZalogowany.Size = new Size(123, 36);
+            lblZalogowany.TabIndex = 2;
+            lblZalogowany.Text = "Anonim";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(0, 0);
+            button1.Name = "button1";
+            button1.Size = new Size(112, 34);
+            button1.TabIndex = 3;
+            button1.Text = "button1";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // btnWyloguj
+            // 
+            btnWyloguj.Font = new Font("Microsoft Sans Serif", 27F, FontStyle.Regular, GraphicsUnit.Point);
+            btnWyloguj.Location = new Point(588, 36);
+            btnWyloguj.Name = "btnWyloguj";
+            btnWyloguj.Size = new Size(110, 78);
+            btnWyloguj.TabIndex = 4;
+            btnWyloguj.Text = "⮜";
+            btnWyloguj.UseVisualStyleBackColor = true;
+            // 
+            // TasksControl
+            // 
+            AutoScaleDimensions = new SizeF(10F, 25F);
+            AutoScaleMode = AutoScaleMode.Font;
+            Controls.Add(btnWyloguj);
+            Controls.Add(button1);
+            Controls.Add(lblZalogowany);
+            Controls.Add(lblZalogowanyText);
+            Controls.Add(lblTytul);
+            Name = "TasksControl";
+            Size = new Size(735, 990);
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblTytul;
+        private Label lblZalogowanyText;
+        private Label lblZalogowany;
+        private Button button1;
+        private Button btnWyloguj;
     }
 }
