cc01606afe1347875b4fc7da0451952c244c57ac
Marcin Zawadzki <nicram01@tlen.pl>
2024-06-08 16:52:32 +0200

    Add new classes

---
+++ WinFormsApp1/Klasy/UserManager.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaZadan.Klasy
+{
+    internal class UserManager
+    {
+        private List<User> users;
+
+        public UserManager()
+        {
+
+        }
+    }
+}
